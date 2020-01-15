95ae82594882e67d28211e44436104c1a917fee0
jvvprasad <jvvprasad04@gmail.com>
2020-01-15 11:03:03 +0530

    first commit

---
+++ precache-manifest.00ff8d07697df3988dc7e6f4876ec799.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a421154751020f67f5e9b696997ca5d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2273e89d11e64fcc302",
+    "url": "/static/css/main.568d2850.chunk.css"
+  },
+  {
+    "revision": "e81946ed29be78e7ed4f",
+    "url": "/static/js/2.1ab8bfbb.chunk.js"
+  },
+  {
+    "revision": "ab2d0b34f5846f9464f0c0eeab6508c0",
+    "url": "/static/js/2.1ab8bfbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2273e89d11e64fcc302",
+    "url": "/static/js/main.5264c5a8.chunk.js"
+  },
+  {
+    "revision": "7c75b6674d189ac0f64e",
+    "url": "/static/js/runtime-main.7cbcfcee.js"
+  },
+  {
+    "revision": "53aa6818fc1047ad0f3b262e3be80e2e",
+    "url": "/static/media/code-fork.53aa6818.svg"
+  }
+]);
